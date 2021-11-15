3fa52430389b2fe423b1c68f68a8c3c401e6a902
Galatan Iulia <iulia.iulia.ig@gmail.com>
2021-11-15 14:31:33 +0200

    added lab8 deliverable1

---
+++ Grammar-Lab8/grammar.py
@@ -0,0 +1,91 @@
+import re
+
+
+class Grammar:
+    def __init__(self, filename):
+        self.__filename = filename
+        self.__nonterminals = []
+        self.__startingSymbol = None
+        self.__alphabet = []
+        self.__productions = {}
+        self.readFromFile()
+
+    def readFromFile(self):
+        file = open(self.__filename, 'r')
+        line = file.readline().strip()
+
+        # read the nonterminals
+        delimiters = "=", "{", "}"
+        regexPattern = '|'.join(map(re.escape, delimiters))
+        tokens = re.split(regexPattern, line)
+
+        nonterminals = tokens[2].split(",")
+
+        for nont in nonterminals:
+            self.__nonterminals.append(nont)
+
+        #read the alphabet
+        line = file.readline().strip()
+        delimiters = "=", "{", "}"
+        regexPattern = '|'.join(map(re.escape, delimiters))
+        tokens = re.split(regexPattern, line)
+        alphabet = tokens[2].split(",")
+
+        for alpha in alphabet:
+            self.__alphabet.append(alpha)
+
+        # read the starting symbol
+        line = file.readline().strip()
+        token = line.split("=")
+        self.__startingSymbol = token[1]
+
+        line = file.readline().strip()
+        # read the productions
+        for line in file:
+            line = line.strip()
+            delimiters = "->"
+            regexPattern = '|'.join(map(re.escape, delimiters))
+            tokens = re.split(regexPattern, line)
+            if tokens[2] == '':
+                tokens[2] = '-'
+
+            if tokens[0] not in self.__productions.keys():
+                self.__productions[tokens[0]] = [tokens[2]]
+            else:
+                self.__productions[tokens[0]].append(tokens[2])
+
+
+
+
+
+    def getNonTerms(self):
+        return self.__nonterminals
+
+    def getStartingSymb(self):
+        return self.__startingSymbol
+
+    def getAlphabet(self):
+        return self.__alphabet
+
+    def getProductions(self):
+        return self.__productions
+
+    def getProdForNonTerm(self, nonTerminal):
+        if nonTerminal not in self.__productions.keys():
+            return []
+        return self.__productions[nonTerminal]
+
+
+    def checkCFG(self):
+        for key in self.__productions.keys():
+            if key not in self.__nonterminals:
+                return False
+            left = self.__productions[key]
+            for t in left:
+                t = t.split(" ")
+                for symbol in t:
+                    if symbol != "":
+                        if symbol not in self.__nonterminals and symbol not in self.__alphabet and symbol != "epsilon":
+                            return False
+        return True
+
---
+++ Grammar-Lab8/main.py
@@ -0,0 +1,33 @@
+from grammar import *
+def menu():
+    print("0. Exit")
+    print("1. Set of NonTerminals")
+    print("2. The alphabet")
+    print("3. Starting Symbol")
+    print("4. Productions")
+    print("5. Get productions for a given non-terminal")
+
+
+if __name__ == '__main__':
+    print("Test Grammar")
+    menu()
+
+    option=-1
+
+
+    grammar = Grammar("g1.in")
+
+    print(grammar.checkCFG())
+    while(option!=0):
+        option=int(input("option:"))
+        if option==1:
+            print(grammar.getNonTerms())
+        elif option==2:
+            print(grammar.getAlphabet())
+        elif option==3:
+            print(grammar.getStartingSymb())
+        elif option==4:
+            print(grammar.getProductions())
+        elif option==5:
+            nonterm = input("non-terminal")
+            print(grammar.getProdForNonTerm(nonterm))
